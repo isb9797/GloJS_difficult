c9a63acb1bb1a8ef30e1b5a9e635672a50f8b6b2
Sergey <isb9797@gmail.com>
2020-04-04 13:52:05 +0300

    Lesson 5 difficult complite

--- js/common.js
+++ js/common.js
@@ -1,28 +1,47 @@
-//Сложное задание к четвертому уроку
+//Сложное задание к пятому уроку
 "use strict";
 
-let strInput = prompt("Введите строку и посмотрите в консоль");
+function arrayWork() {
+  const arrMehods = {
+    //Можно вводить и вручную, но я решил, что так быстрее)
+    arr: ["1", "2", "3", "4", "5", "6", "7", "43", "81", "83"],
 
-let strFunc = argument => {
-  let type = +argument * 10;
+    arraySlice: function () {
+      let sliceArrItem;
 
-  let str = "";
+      for (let i in this.arr) {
+        sliceArrItem = this.arr[i];
+        sliceArrItem = +sliceArrItem.slice(0, 1);
 
-  if (!isNaN(type)) {
-    console.log(
-      "Вы уверены, что написали предложение? Это больше похоже на число. Читайте внимательно)"
-    );
-  } else {
-    str = argument.trim();
-    let subStr = "";
+        if (sliceArrItem === 2 || sliceArrItem === 4) {
+          console.log(this.arr[i]);
+        }
+      }
+    },
+    arrayNatural: function () {
+      let arrNat;
 
-    if (str.length > 30) {
-      subStr = str.substr(0, 30) + " ...";
-      console.log(subStr);
-    } else {
-      console.log(str);
-    }
-  }
-};
+      for (let num = 0; num <= 100; num++) {
+        // arrNat = +this.arr[i];
 
-strFunc(strInput);
+        nextPrime: for (let j = 2; j <= num; j++) {
+          for (let k = 2; k < num; k++) {
+            if (j % k === 0) {
+              continue nextPrime;
+            }
+          }
+          console.log(num + ":    " + "Делители: " + j + ", " + 1);
+        }
+      }
+    },
+  };
+
+  //Вывод числел, начинающихся с 2 и 4
+  arrMehods.arraySlice();
+
+  console.log("Простые числа");
+
+  arrMehods.arrayNatural();
+}
+
+arrayWork();
